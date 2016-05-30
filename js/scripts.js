e8c9188cdbc502684d08088adc3cf929f83e9062
Adria Marcet <adriamarcetrovira@gmail.com>
2016-05-30 01:15:47 -0400

    Started some scripting

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-//This is DoingSome.js code
+var increaseCount = function(){
+	console.log( "has clicao una carta y esto es increaseCount ");
+	
+}
+
+// this is the document.ready code
 $(document).ready( function(){
-	console.log("hola caracola");
+
+	// store all cards displayed in a var
+	var tile =  $('.card');
+
+		// the number of tiles that can be flipped
+		var tilesFliped = 2;
+
+		// Do something for each card
+		$(tile).each( function(){
+
+			// on click on each
+			$(this).on( "click", function(){
+				// do the function add +1 to counter
+				increaseCount();
+
+			});
+		});
+	
+
+	console.log("Ehh, What's Up Doc? I see you're ready. (-‿‿-)");
 });
